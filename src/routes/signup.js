8b5f2efe808ac58898396e850ebda8b1d5db6f38
Varma390 <119935863+Varma390@users.noreply.github.com>
2022-12-28 19:31:35 +0530

    Update signup.js

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -86,45 +86,4 @@ route.post('/register', async (req, res) => {
 });
 module.exports= route;
 
- /* route.post('/register',async (req,res)=>{
-    try{
-       console.log(req.body)
-        
-    let user = await login.findOne({email:req.body.email})
-    console.log(user,1)
-    if(user){
-       return  res.status(409).json({
-            status:'failure',
-            message:'user already exists with the given email'
-        })
-    }
-   
-    // bcrypt.hash(password,10,async function(err,hash){
-    //     if(err){
-    //         return res.status(500).json({
-            
-    //             status:'failed',
-    //             message:err.message
-    //         })
-    //     }
-    console.log("going")
-    const use = await login.create({
-        
-        email:req.body.email,
-        password:req.body.password
-    });
-    
-    res.json({
-        status:'sucesss',
-        message:use})
-    
-    }
-    catch(e){
-        res.json({
-            status:'failure',
-            message:e.message
-        })
-    }
-
- })
- */
+ 
